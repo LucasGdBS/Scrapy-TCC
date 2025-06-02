ca797179b4f79b7b739fa42367733bb9944627bd
LucasGdbs <lgbs@cesar.school>
2025-06-01 23:59:17 -0300

    refactor: create scrapy class

---
+++ app/agents/scrapy.py
@@ -0,0 +1,36 @@
+from language_models.llm import LLM
+from pathlib import Path
+import importlib.util
+import hashlib
+
+class Scrapy:
+    def __init__(self, llm: LLM, folder: Path | str = Path(f'./scrapers')):
+        self.llm = llm
+        self.folder = Path(folder)
+
+    def __generate_hash(self, url: str, prompt: str):
+        unique_key = url + prompt
+        return hashlib.sha256(unique_key.encode()).hexdigest()
+    
+    def __load_and_run_func(self, filepath: Path):
+        spec = importlib.util.spec_from_file_location("scraper", filepath)
+        scraper = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(scraper)
+        return scraper.run()
+
+    def scrapy(self, static_site_url: str, extract_prompt: str):
+        hash = self.__generate_hash(static_site_url, extract_prompt)
+        file_path = self.folder / f'{hash}.py'
+
+        if file_path.exists():
+            result = self.__load_and_run_func(file_path)
+            print(result)
+        else:
+            try_again = 'Y'
+            while try_again == 'Y':
+                self.llm.generate_code(file_path=file_path, static_site_url=static_site_url, prompt=extract_prompt)
+                result = self.__load_and_run_func(file_path)
+                print(result)
+
+                try_again = input("Deseja criar outro script? Y/N: ").upper()
+    
--- app/main.py
+++ app/main.py
@@ -1,76 +1,29 @@
 from decouple import config as env
-from pathlib import Path
-import importlib.util
-import hashlib
-from functools import wraps
-import time
 from language_models.gemini import Gemini
+from agents.scrapy import Scrapy
 
 GEMINI_API_KEY = env('GEMINI_API_KEY')
+gemini = Gemini(
+    api_key=GEMINI_API_KEY,
+    system_instruction=(
+        "You are a Python code generator.\n"
+        "Respond exclusively with code.\n"
+        "No comments, no explanations, no markdown, no extra text.\n"
+        "Only pure, valid, and executable code.\n"
+        "Create a function named 'run' that executes the code and returns the data.\n"
+        "Do not forget to set the User-Agent header.\n"
+        "Return the data in dict format.\n"
+        "Generate scraping code for:"
+    )
+)
 
-def timeit(func):
-    """
-    Decorator that measures and prints the execution time of the decorated function.
-    Args:
-        func (callable): The function whose execution time is to be measured.
-    Returns:
-        callable: A wrapper function that executes the original function, prints its execution time, and returns its result.
-    Example:
-        @timeit
-        def my_function():
-            # function body
-    """
-    @wraps(func)
-    def timeit_wrapper(*args, **kwargs):
-        start_time = time.perf_counter()
-        result = func(*args, **kwargs)
-        end_time = time.perf_counter()
-        total_time = end_time - start_time
-        print(f'\033[93mFunction {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds\033[0m')
-        return result
-    return timeit_wrapper
-
-def generate_hash(url: str, prompt: str):
-    unique_key = url + prompt
-    return hashlib.sha256(unique_key.encode()).hexdigest()
-
-def load_and_run_scrapper(filepath: Path):
-    spec = importlib.util.spec_from_file_location("scrapper", filepath)
-    scraper = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(scraper)
-    return scraper.run()
+scrapy = Scrapy(gemini)
 
 # Script
-def script():
-    gemini = Gemini(
-        api_key=GEMINI_API_KEY,
-        system_instruction=(
-            "You are a Python code generator.\n"
-            "Respond exclusively with code.\n"
-            "No comments, no explanations, no markdown, no extra text.\n"
-            "Only pure, valid, and executable code.\n"
-            "Create a function named 'run' that executes the code and returns the data.\n"
-            "Do not forget to set the User-Agent header.\n"
-            "Return the data in dict format.\n"
-            "Generate scraping code for:"
-        )
-    )
+def main():
     static_site_url = input("Coloque a URL do site estatico que você quer raspar: ")
     extract_prompt = input("Explique quais dados você quer: ")
 
-    hash = generate_hash(static_site_url, extract_prompt)
-    file_path = Path(f'./scrapers/{hash}.py')
+    scrapy.scrapy(static_site_url, extract_prompt)
 
-    if file_path.exists():
-        result = load_and_run_scrapper(file_path)
-        print(result)
-    else:
-        try_again = 'Y'
-        while try_again == 'Y':
-            gemini.generate_code(file_path=file_path, static_site_url=static_site_url, prompt=extract_prompt)
-            result = load_and_run_scrapper(file_path)
-            print(result)
-
-            try_again = input("Deseja criar outro script? Y/N: ").upper()
-
-script()
+main()
